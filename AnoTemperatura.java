d7052a73019ca4a1674883aae25877adbac79b8a
Gabriel Machado <127336276+P3RALT@users.noreply.github.com>
2025-05-30 10:44:10 -0300

    Add files via upload

---
+++ AnoTemperatura.java
@@ -0,0 +1,18 @@
+package lista07;
+
+public class AnoTemperatura {
+   public String mes;
+   public double temperatura; 
+
+   public AnoTemperatura(String mes, double temperatura){
+   this.mes = mes;
+   this.temperatura = temperatura;
+   }
+   
+   public String getmes(){
+   return mes;}
+   
+   public double gettemperatura(){
+   return temperatura;}
+   
+}
